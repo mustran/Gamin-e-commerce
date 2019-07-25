b100dde97922d2699156b5507a582a5858578ee9
mustran <ademimusli000@gmail.com>
2019-07-25 06:33:30 +0200

    routes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import {Switch, Route} from 'react-router-dom';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+import ProductList from "./components/ProductList";
+
 class App extends Component {
     render() {
         return (
             <React.Fragment>
-                <h3>Hello from app</h3>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/" component={ProductList}></Route>
+                    <Route path="/details" component={Details}></Route>
+                    <Route path="/cart" component={Cart}></Route>
+                    <Route component={Default}></Route>
+                </Switch>
             </React.Fragment>
         );
     }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello from Navbar</h3>
-            </div>
-        )
+            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                <Link to="/">
+                    <img height="50px" src={logo} alt="logo" className="navbar-brand" />
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">
+                            Products
+                        </Link>
+                    </li>
+                </ul>
+                <Link to="/cart" className="ml-auto">
+                    <button>
+                        <i className="fa fa-cart-plus"/>
+                        My Cart
+                    </button>
+                </Link>
+            </nav>
+        );
     }
 }
