8600c1112674647a623514a7363c0f89e9649ee8
mustran <ademimusli000@gmail.com>
2019-07-27 19:33:40 +0200

    increment/decrepemt product in cart + page not found added

--- src/context.js
+++ src/context.js
@@ -77,11 +77,40 @@ class ProductProvider extends Component {
     };
 
     increment = id => {
-        console.log("increment method");
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count + 1;
+        product.total = product.count * product.price;
+
+        this.setState({
+            cart: [...tempCart]
+        }, ()=> {
+            this.addTotals();
+        })
     };
 
     decrement = id => {
-        console.log("decrement method");
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count - 1;
+
+        if(product.count === 0){
+            this.removeItem(id);
+        }
+        else{
+            product.total = product.count * product.price;
+            this.setState({
+                cart: [...tempCart]
+            }, ()=> {
+                this.addTotals();
+            })
+        }
     };
 
     removeItem = id => {
