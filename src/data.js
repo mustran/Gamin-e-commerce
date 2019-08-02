f2c21464894854718a10315a60f4c86c47d50699
mustran <ademimusli000@gmail.com>
2019-08-02 21:40:13 +0200

    products changed

--- src/data.js
+++ src/data.js
@@ -1,111 +1,116 @@
 export const storeProducts = [
-  {
+    {
+        id: 1,
+        title: "CUK Stratos Case",
+        img: "img-gaming/Untitled-1.png",
+        price: 159,
+        company: "CUKUSA",
+        info:
+            "Control the look of your desktop to fit your mood with 7 different RGB fan color options via reset button on the ride side of the case.Hard Drive Support: 4 x 2.5 SSD/HDD, or 2 x 3.5 Hard Drive + 2 x 2.5 SSD/HDD | ATX, Micro-ATX, Mini-ITX board support. Pre-Installed Ports: 2 x USB 3.0, 7 x Expansion Slots. CPU Cooling Fan Height Support: 180mm Max | Graphics Card Dimensions Support: 400mm length, 170mm height. Note: Power Supply is not included and is necessary for RGB Fans functionality",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "Arctis 7",
+        img: "img-gaming/Untitled-2.png",
+        price: 179,
+        company: "SteelSeries",
+        info:
+            "Best wireless gaming headset — PC Gamer" +
+            " Lossless 2.4 GHz wireless audio designed for low latency gaming " +
+            "Best mic in gaming: the Discord-certified Clearcast bidirectional microphone " +
+            "Hear stunning detail in all games with award-winning Arctis sound" +
+            "Next-generation DTS Headphone:X v2.0 surround sound" +
+            "24-hour battery life outlasts even your longest gaming sessions",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        title: "Corsair Dominator Platinum",
+        img: "img-gaming/Untitled-3.png",
+        price: 729,
+        company: "CORSAIR",
+        info:
+            "Iconic and Refined High-Performance RGB Memory. High Speed and Tight Timings. Premium Craftsmanship. 12 Ultra-Bright RGB LEDs Per Module. Patented Dual-Channel DHX Cooling Technology",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        title: "Dell 27 Monitor: S2719DGF",
+        img: "img-gaming/Untitled-4.png",
+        price: 379,
+        company: "DELL",
+        info:
+            "Slay any beast with your very own. Dominate with crisp QHD resolution and tear-free visuals enabled by FreeSync",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 5,
+        title: "GEFORCE GTX 1080",
+        img: "img-gaming/Untitled-5.png",
+        price: 550,
+        company: "NVIDIA",
+        info:
+            "NVIDIA's GeForce GTX 1080 is powered by the award-winning NVIDIA Pascal™ architecture. Discover amazing performance, power efficiency, and gaming experiences. This is the ultimate gaming platform. #GameReady.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 6,
+        title: "APEX M750",
+        img: "img-gaming/Untitled-6.png",
+        price: 139,
+        company: "STEELSERIES",
+        info:
+            "The Apex M750 combines showstopping lighting effects and crucial in-game notifications with sleek aluminum construction and durable SteelSeries QX2 mechanical switches.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 7,
+        title: "Rival 110",
+        img: "img-gaming/Untitled-7.png",
+        price: 39,
+        company: "STEELSERIES",
+        info:
+            "Rival 110 is a streamlined, pure performance gaming mouse engineered with the new TrueMove1 optical sensor for 1 to 1 tracking and guaranteed 30-million click mechanical switches for lifelong durability.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 8,
+        title: "Rival 650",
+        img: "img-gaming/Untitled-8.png",
+        price: 129,
+        company: "Steelseries",
+        info:
+            "Exclusive Quantum WirelessTM offers lag-free 1000Hz (1ms) gaming. 15-minutes of fast charging delivers 10+ hours of game play. True 1 to 1 tracking precision with TrueMove3 optical sensor. Dedicated secondary depth sensor for instant lift off detection. 256 center of gravity weight tuning configurations",
+        inCart: false,
+        count: 0,
+        total: 0
+    }
+];
+
+export const detailProduct = {
     id: 1,
     title: "Google Pixel - Black",
     img: "img/product-1.png",
     price: 10,
-    company: "GOOGLE",
+    company: "google",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+        "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
     inCart: false,
     count: 0,
     total: 0
-  },
-  {
-    id: 2,
-    title: "Samsung S7",
-    img: "img/product-2.png",
-    price: 16,
-    company: "SAMSUNG",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 3,
-    title: "HTC 10 - Black",
-    img: "img/product-3.png",
-    price: 8,
-    company: "htc",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 4,
-    title: "HTC 10 - White",
-    img: "img/product-4.png",
-    price: 18,
-    company: "htc",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 5,
-    title: "HTC Desire 626s",
-    img: "img/product-5.png",
-    price: 24,
-    company: "htc",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 6,
-    title: "Vintage Iphone",
-    img: "img/product-6.png",
-    price: 17,
-    company: "apple",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 7,
-    title: "Iphone 7",
-    img: "img/product-7.png",
-    price: 30,
-    company: "apple",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 8,
-    title: "Smashed Iphone",
-    img: "img/product-8.png",
-    price: 2,
-    company: "apple",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  }
-];
-
-export const detailProduct = {
-  id: 1,
-  title: "Google Pixel - Black",
-  img: "img/product-1.png",
-  price: 10,
-  company: "google",
-  info:
-    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-  inCart: false,
-  count: 0,
-  total: 0
 };
